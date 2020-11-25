be72b315627522775f611e506abfc8d26db5b021
Bintang2412 <74962335+Bintang2412@users.noreply.github.com>
2020-11-25 22:23:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const options = require('./options')
 // AUTO UPDATE BY NURUTOMO
 // THX FOR NURUTOMO
 // Cache handler and check for file change
-require('./tobz.js')
-nocache('./tobz.js', module => console.log(`'${module}' Updated!`))
+require('./binn.js')
+nocache('./binn.js', module => console.log(`'${module}' Updated!`))
 
 const adminNumber = JSON.parse(fs.readFileSync('./lib/admin.json'))
 const setting = JSON.parse(fs.readFileSync('./lib/setting.json'))
@@ -25,84 +25,84 @@ let {
     restartState: isRestart
     } = setting
 
-function restartAwal(tobz){
+function restartAwal(binn){
     setting.restartState = false
     isRestart = false
-    tobz.sendText(setting.restartId, 'Restart Succesfull!')
+    binn.sendText(setting.restartId, 'Restart Succesfull!')
     setting.restartId = 'undefined'
     //fs.writeFileSync('./lib/setting.json', JSON.stringify(setting, null,2));
 }
 
-const start = async (tobz = new Client()) => {
+const start = async (binn = new Client()) => {
         console.log('------------------------------------------------')
-        console.log(color(figlet.textSync('ELAINA BOT', { horizontalLayout: 'full' })))
+        console.log(color(figlet.textSync('BM-BOT', { horizontalLayout: 'full' })))
         console.log('------------------------------------------------')
-        console.log('[DEV] TOBZ')
+        console.log('[DEV] BINN')
         console.log('[SERVER] Server Started!')
-        tobz.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
+        binn.onAnyMessage((fn) => messageLog(fn.fromMe, fn.type))
         // Force it to keep the current session
-        tobz.onStateChanged((state) => {
+        binn.onStateChanged((state) => {
             console.log('[Client State]', state)
-            if (state === 'CONFLICT' || state === 'UNLAUNCHED') tobz.forceRefocus()
+            if (state === 'CONFLICT' || state === 'UNLAUNCHED') binn.forceRefocus()
         })
         // listening on message
-        tobz.onMessage((async (message) => {
+        binn.onMessage((async (message) => {
 
-        tobz.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
+        binn.getAmountOfLoadedMessages() // Cut message Cache if cache more than 3K
             .then((msg) => {
                 if (msg >= 1000) {
                     console.log('[CLIENT]', color(`Loaded Message Reach ${msg}, cuting message cache...`, 'yellow'))
-                    tobz.cutMsgCache()
+                    binn.cutMsgCache()
                 }
             })
         // Message Handler (Loaded from recent cache)
-        require('./tobz.js')(tobz, message)
-        require('./lib/help.js')(tobz, message)
+        require('./binn.js')(binn, message)
+        require('./lib/help.js')(binn, message)
     }))
            
 
-        tobz.onGlobalParicipantsChanged((async (heuh) => {
-            await welcome(tobz, heuh) 
-            left(tobz, heuh)
+        binn.onGlobalParicipantsChanged((async (heuh) => {
+            await welcome(binn, heuh) 
+            left(binn, heuh)
             }))
         
-        tobz.onAddedToGroup(async (chat) => {
-            if(isWhite(chat.id)) return tobz.sendText(chat.id, 'Halo aku Elaina, Ketik #help Untuk Melihat List Command Ku...')
+        binn.onAddedToGroup(async (chat) => {
+            if(isWhite(chat.id)) return binn.sendText(chat.id, 'Halo aku BM-BOT, Ketik #help Untuk Melihat List Command Ku...')
             if(mtcState === false){
-                const groups = await tobz.getAllGroups()
+                const groups = await binn.getAllGroups()
                 // BOT group count less than
                 if(groups.length > groupLimit){
-                    await tobz.sendText(chat.id, 'Maaf, Batas group yang dapat Elaina tampung sudah penuh').then(async () =>{
-                        tobz.deleteChat(chat.id)
-                        tobz.leaveGroup(chat.id)
+                    await binn.sendText(chat.id, 'Maaf, Batas group yang dapat BM-BOT tampung sudah penuh').then(async () =>{
+                        binn.deleteChat(chat.id)
+                        binn.leaveGroup(chat.id)
                     })
                 }else{
                     if(chat.groupMetadata.participants.length < memberLimit){
-                        await tobz.sendText(chat.id, `Maaf, BOT keluar jika member group tidak melebihi ${memberLimit} orang`).then(async () =>{
-                            tobz.deleteChat(chat.id)
-                            tobz.leaveGroup(chat.id)
+                        await binn.sendText(chat.id, `Maaf, BOT keluar jika member group tidak melebihi ${memberLimit} orang`).then(async () =>{
+                            binn.deleteChat(chat.id)
+                            binn.leaveGroup(chat.id)
                         })
                     }else{
-                        if(!chat.isReadOnly) tobz.sendText(chat.id, 'Halo aku Elaina, Ketik #help Untuk Melihat List Command Ku...')
+                        if(!chat.isReadOnly) binn.sendText(chat.id, 'Halo aku BM-BOT, Ketik #help Untuk Melihat List Command Ku...')
                     }
                 }
             }else{
-                await tobz.sendText(chat.id, 'Elaina sedang maintenance, coba lain hari').then(async () => {
-                    tobz.deleteChat(chat.id)
-                    tobz.leaveGroup(chat.id)
+                await binn.sendText(chat.id, 'BM-BOT sedang maintenance, coba lain hari').then(async () => {
+                    binn.deleteChat(chat.id)
+                    binn.leaveGroup(chat.id)
                 })
             }
         })
 
-        /*tobz.onAck((x => {
+        /*binn.onAck((x => {
             const { from, to, ack } = x
-            if (x !== 3) tobz.sendSeen(to)
+            if (x !== 3) binn.sendSeen(to)
         }))*/
 
         // listening on Incoming Call
-        tobz.onIncomingCall(( async (call) => {
-            await tobz.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block!.\nJika ingin membuka block harap chat Owner!')
-            .then(() => tobz.contactBlock(call.peerJid))
+        binn.onIncomingCall(( async (call) => {
+            await binn.sendText(call.peerJid, 'Maaf, saya tidak bisa menerima panggilan. nelfon = block!.\nJika ingin membuka block harap chat Owner!')
+            .then(() => binn.contactBlock(call.peerJid))
         }))
     }
 
@@ -135,5 +135,5 @@ function uncache(module = '.') {
 }
 
 create(options(true, start))
-    .then(tobz => start(tobz))
+    .then(binn => start(binn))
     .catch((error) => console.log(error))
